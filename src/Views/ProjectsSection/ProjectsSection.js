db55b42a6206e525c0e02a87392711bcc6cf9e1f
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2024-02-02 15:43:32 +0100

    push

--- src/Views/ProjectsSection/ProjectsSection.js
+++ src/Views/ProjectsSection/ProjectsSection.js
@@ -3,63 +3,76 @@ import React, { useEffect } from "react";
 import "aos/dist/aos.css"; // Import AOS CSS
 import AOS from "aos";
 import MasonryGrid from "./Components/MasonryGrid";
-import Winetour from "../../Assets/ProjectPhotos/borvidek.jpg"
-import Food4Me from "../../Assets/ProjectPhotos/food4me.jpg"
-import CatsleSearcher from "../../Assets/ProjectPhotos/kastelykereso.jpg"
-import Foodemy from "../../Assets/ProjectPhotos/foodemy.jpg"
-import Seatwave from "../../Assets/ProjectPhotos/seatwave.jpg"
-import Ecommerce from "../../Assets/ProjectPhotos/ecommerce.jpg"
+import Winetour from "../../Assets/ProjectPhotos/borvidek.jpg";
+import Food4Me from "../../Assets/ProjectPhotos/food4me.jpg";
+import CatsleSearcher from "../../Assets/ProjectPhotos/kastelykereso.jpg";
+import Foodemy from "../../Assets/ProjectPhotos/foodemy.jpg";
+import Seatwave from "../../Assets/ProjectPhotos/seatwave.jpg";
+import Ecommerce from "../../Assets/ProjectPhotos/ecommerce.jpg";
+import WorkWave from "../../Assets/ProjectPhotos/workWave.jpg"
 export const ProjectsSection = () => {
   const items = [
     {
-      imageSrc: Winetour,
-      name: "Sopron Winetour App",
-      description: "An application made for the Sopron Wine Region Group in Sopron",
-      href:"https://github.com/farkas1070/Winetour",
-      id:'SopronWineTour',
+      imageSrc: Ecommerce,
+      name: "Shuuz",
+      description: "An Online Shoe E-commrce platform, Made with React and .NET CORE",
+      href: "https://github.com/farkas1070/EcommerceProject",
+      id: "EMedicine",
     },
     {
-      imageSrc:Ecommerce,
-      name: "Shuuz",
-      description: "An Online Shoe E-commrce platform",
-      href:"https://github.com/farkas1070/EcommerceProject",
-      id:'EMedicine',
+      imageSrc: Winetour,
+      name: "Sopron Winetour App",
+      description:
+        "An application made for the Sopron Wine Region Group in Sopron, Made with React Native and Strapi",
+      href: "https://github.com/farkas1070/Winetour",
+      id: "SopronWineTour",
     },
-    
     {
       imageSrc: Food4Me,
       name: "Food4Me",
-      description: "A Fitness, Social Media, And short vide-viewing application",
-      href:"https://github.com/farkas1070/food4me",
-      id:'food4Me',
+      description:
+        "A Fitness, Social Media, And short vide-viewing application, Made with React Native and Firebase",
+      href: "https://github.com/farkas1070/food4me",
+      id: "food4Me",
     },
     {
       imageSrc: Seatwave,
       name: "Seatwave",
-      description: "A Cinema Booking Website",
-      href:"https://github.com/farkas1070/MovieSeatTrackerApp",
-      id:'Seatwave',
+      description: "A Cinema Booking Website Made with React and Firebase",
+      href: "https://github.com/farkas1070/MovieSeatTrackerApp",
+      id: "Seatwave",
     },
     {
       imageSrc: CatsleSearcher,
       name: "CastleSearcher",
-      description: "A website dedicated to storing images taken in past vacations",
-      href:"https://github.com/farkas1070/FamiliyGeoLocator",
-      id:'CastleSearcher',
+      description:
+        "A website dedicated to storing images taken in past vacations, made with React and Firebase",
+      href: "https://github.com/farkas1070/FamiliyGeoLocator",
+      id: "CastleSearcher",
     },
-    
+
     {
       imageSrc: Foodemy,
       name: "Foodemy",
-      description: "An Online Food delivery platform with an E-commerce like functionality",
-      href:"https://github.com/farkas1070/MovieSeatTrackerApp",
-      id:'Foodemy',
-    }
+      description:
+        "An Online Food delivery platform made with Angular and Nodejs",
+      href: "https://github.com/farkas1070/MovieSeatTrackerApp",
+      id: "Foodemy",
+    },
+    {
+      imageSrc: WorkWave,
+      name: "WorkWave",
+      description: "A job listing website made with Laravel and Angular",
+      href: "https://github.com/farkas1070/TailwindComponents",
+      id: "WorkWave",
+    },
     // Add more items as needed
   ];
   return (
-    <div id="portfolio-section" className="flex flex-col w-full items-center bg-[#212024] p-8">
-      
+    <div
+      id="portfolio-section"
+      className="flex flex-col w-full items-center bg-[#212024] p-8"
+    >
       <div className="w-full flex flex-col items-center ">
         <h1
           data-aos="fade-up"
@@ -67,8 +80,11 @@ export const ProjectsSection = () => {
         >
           My Portfolio
         </h1>
-        <h2 data-aos="fade-up" class="text-lg text-gray-300 mb-[20px] lg:mb-[60px] text-center">
-        See some of the projects I have created over the years
+        <h2
+          data-aos="fade-up"
+          class="text-lg text-gray-300 mb-[20px] lg:mb-[60px] text-center"
+        >
+          See some of the projects I have created over the years
         </h2>
       </div>
 
